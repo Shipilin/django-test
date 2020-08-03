3ca67eac0aad25d9a484ec7dfdc4174fda4de17c
Mikhail Shipilin <ship.yar@gmail.com>
2020-08-03 16:26:46 +0200

    Initial commit

---
+++ app4/urls.py
@@ -0,0 +1,13 @@
+from app4 import views
+from django.urls import path
+
+# Template tagging
+app_name = 'app4'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('registration/', views.registration, name='registration'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('special/', views.special, name='special'),
+]
